b83ddde32a5b57f37457a7a1a75ae7bfa59dbceb
abduvali <abduvalikhaydarov005@gmail.com>
2024-05-19 14:55:21 +0500

    kommit

--- components/shared/Logo.tsx
+++ components/shared/Logo.tsx
@@ -1,22 +1,16 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Logo() {
-  return (
-    <Link
-      href={"/"}
-      className="text-current/80 text-3xl uppercase font-bold flex items-center pt-2"
-    >
-      <Image
-        src={"/logo.png"}
-        width={60}
-        height={60}
-        className="object-cover"
-        alt="Logo"
-      />
-      Adore
-    </Link>
-  );
+	return (
+		<Link
+			href={'/'}
+			className="text-current/80 text-3xl uppercase font-bold flex items-center pt-2 gap-3"
+		>
+			<Image src={'/logo.png'} width={50} height={50} className="object-cover" alt="Logo" />
+			Amore
+		</Link>
+	);
 }
