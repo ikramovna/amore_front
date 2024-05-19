d34aae5688b8fe87d9831fc52d61c2593dea271c
abbosbek23 <muratovabbosbek@gmail.com>
2024-05-19 15:19:38 +0500

    change products

--- components/shared/Logo.tsx
+++ components/shared/Logo.tsx
@@ -5,12 +5,10 @@ import Link from 'next/link';
 
 export default function Logo() {
 	return (
-		<Link
-			href={'/'}
-			className="text-current/80 text-3xl uppercase font-bold flex items-center pt-2 gap-3"
-		>
+		<Link href={'/'}
+			className="text-current/80 text-3xl uppercase font-bold flex items-center pt-2 gap-3">
 			<Image src={'/logo.png'} width={50} height={50} className="object-cover" alt="Logo" />
 			Amore
-		</Link>
+		</Linkhref=>
 	);
 }
